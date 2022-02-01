7f44c9c8d5ed0c3bf906409b251aae714fce4e91
exydis01 <97627257+exydis01@users.noreply.github.com>
2022-02-01 17:18:32 -0500

    assignment

---
+++ Player/Player.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var velocity = Vector2.ZERO
+var rotation_speed = 5.0
+var speed = 0.1
+var max_speed = 10
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+func _physics_process(delta):
+		position = position +velocity
+		velocity = velocity.normalized() + clamp(velocity.length(), 0, max_speed)
+		
+		$Exhaust.hide()
+		if Input.is_action_just_pressed("forward"):
+			velocity = velocity + Vector2(0, -speed).rotated(rotation)
+			$Exhaust.show()
+		if Input.is_action_just_pressed("left"):
+			rotation_degrees = rotation_degrees - rotation_speed
+		if Input.is_action_just_pressed("right"):
+			rotation_degrees = rotation_degrees + rotation_speed
+		position.x = wrapf(position.x, 0, 1024)
+		position.y = wrapf(position.y, 0, 600)
